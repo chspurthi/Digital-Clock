ab94f5f3dbd8a114185aba689bfcfb37e6f00072
chspurthi <chintalaspurthi@gmail.com>
2024-11-19 19:53:42 +0530

    Digital Clock

---
+++ DScript.js
@@ -0,0 +1,76 @@
+
+function clock() {
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var second = today.getSeconds();
+    let period = "AM";
+
+    if (hours >= 12) {
+        period = "PM";
+    }
+
+    hours = hours > 12 ? hours % 12 : hours;
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
+
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".second").innerHTML = second;
+    document.querySelector(".period").innerHTML = period;
+}
+var updateclock = setInterval(clock, 1000);
+
+var today = new Date();
+const dayNumber = today.getDate();
+const year = today.getFullYear();
+const dayName = today.toLocaleDateString("default", { weekday: "long" });
+const monthName = today.toLocaleDateString("default", { month: "short" });
+
+document.querySelector(".month-name").innerHTML = monthName;
+document.querySelector(".day-name").innerHTML = dayName;
+document.querySelector(".day-number").innerHTML = dayNumber;
+document.querySelector(".year").innerHTML = year;
+ // <![CDATA[  <-- For SVG support
+ if ('WebSocket' in window) {
+    (function () {
+        function refreshCSS() {
+            var sheets = [].slice.call(document.getElementsByTagName("link"));
+            var head = document.getElementsByTagName("head")[0];
+            for (var i = 0; i < sheets.length; ++i) {
+                var elem = sheets[i];
+                var parent = elem.parentElement || head;
+                parent.removeChild(elem);
+                var rel = elem.rel;
+                if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+                    var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+                    elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+                }
+                parent.appendChild(elem);
+            }
+        }
+        var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+        var address = protocol + window.location.host + window.location.pathname + '/ws';
+        var socket = new WebSocket(address);
+        socket.onmessage = function (msg) {
+            if (msg.data == 'reload') window.location.reload();
+            else if (msg.data == 'refreshcss') refreshCSS();
+        };
+        if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+            console.log('Live reload enabled.');
+            sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+        }
+    })();
+}
+else {
+    console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+}
+
